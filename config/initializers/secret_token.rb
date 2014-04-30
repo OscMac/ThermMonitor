8c20adfa39de6d70804128f6fb9b3e16240d4bf6
Luis Maldonado <luisoscar.mc@gmail.com>
2014-04-30 19:47:38 -0400

    en_blanco

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThermMonitor::Application.config.secret_key_base = '7c04da888e3d250eba170f86290f20f69b43aa766b973cd4138166f81b72a78318ed27858ec324340fff9abc0786e313712340fddd1ea9c6d91c7aa4e1f61e26'
